43b4b78c637be8d6f8bf784bb52eb6252d991901
Dang Do <dangdo@Mac.lan>
2026-04-20 10:32:38 +0700

    Fix relative paths and base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HPC_Presentation/',
+  base: './',
   plugins: [react()],
   server: {
     allowedHosts: [
