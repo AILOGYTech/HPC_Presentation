edab3d4795f215698c54169e34413474f8eb1d6e
Dang Do <dangdo@Mac.lan>
2026-04-20 11:33:54 +0700

    Trigger deploy

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HPC_Presentation/',
   plugins: [react()],
+  base: './',
   server: {
-    allowedHosts: [
-      '8130-171-250-163-207.ngrok-free.app',
-      '.ngrok-free.app'
-    ]
+    allowedHosts: true
   }
 })
